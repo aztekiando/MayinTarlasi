19b4b34d188c5f1a599f8fd9f341529616fa5aef
aztekiando <aaslanmirza23@gmail.com>
2020-09-16 19:15:00 +0300

    Eksikler Tamamlandı

---
+++ MayinTarlasi/MayinTarlasi/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBaslat = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 56);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(639, 485);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnBaslat
+            // 
+            this.btnBaslat.Location = new System.Drawing.Point(449, 12);
+            this.btnBaslat.Name = "btnBaslat";
+            this.btnBaslat.Size = new System.Drawing.Size(88, 37);
+            this.btnBaslat.TabIndex = 1;
+            this.btnBaslat.Text = "Başlat";
+            this.btnBaslat.UseVisualStyleBackColor = true;
+            this.btnBaslat.Click += new System.EventHandler(this.btnBaslat_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(128, 23);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 22);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(14, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mayın Sayısı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(294, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 541);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btnBaslat);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnBaslat;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MayinTarlasi/MayinTarlasi/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+          ArrayList mayinlar = new ArrayList();
+          int sayi = 0;
+        private void btnBaslat_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            btnBaslat.Enabled = false;
+            numericUpDown1.Enabled = false;
+            Random rast = new Random();
+
+            for (int i = 0; i < Convert.ToInt32(numericUpDown1.Value); i++)  //Mayın üretimi
+            {
+               Uret:
+                sayi = rast.Next(0,64);
+
+
+                if (mayinlar.Contains(sayi))
+                {
+                    goto Uret;
+                }
+                else
+                {
+                    mayinlar.Add(sayi);
+                }
+
+            }
+
+            for (int i = 0; i < 64; i++)  // Button Üretimi
+            {
+
+                Button btn = new Button();
+                btn.Size = new Size(72, 50);
+                btn.FlatStyle = FlatStyle.Flat;
+                flowLayoutPanel1.Controls.Add(btn);
+
+                btn.Click += Btn_Click;
+
+                if (mayinlar.Contains(i))
+                {
+                    btn.Tag = -1;
+                }
+                else
+                {
+                    btn.Tag = rast.Next(1,23);
+                }
+            }
+           
+            
+            
+        }
+        int bitis;
+        int sonuc;
+        int baska;
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            
+            sonuc = 64 - Convert.ToInt32(numericUpDown1.Value);
+
+            Button tiklanan = (Button)sender;
+            tiklanan.Text = tiklanan.Tag.ToString();
+            if (Convert.ToInt32(tiklanan.Tag) == -1)
+            {
+                tiklanan.BackColor = Color.Red;
+                label1.Text = "0";
+                
+
+                for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+                {
+                    flowLayoutPanel1.Controls[i].Enabled = false;
+                    if (Convert.ToInt32(flowLayoutPanel1.Controls[i].Tag) == -1)
+                    {
+                        flowLayoutPanel1.Controls[i].Text = "";
+                        flowLayoutPanel1.Controls[i].BackColor = Color.Red;
+                        
+                        
+                    }
+                    else
+                    {
+                        flowLayoutPanel1.Controls[i].Text = flowLayoutPanel1.Controls[i].Tag.ToString();
+                        
+                    }
+
+                }
+
+                btnBaslat.Enabled = true;
+                numericUpDown1.Enabled = true;
+            }
+            else
+            {
+                tiklanan.Enabled = false;
+                tiklanan.BackColor = Color.Green;
+
+
+                //tiklanan.Text = tiklanan.Tag.ToString();
+
+
+                baska = baska + Convert.ToInt32(tiklanan.Text);
+                string skor = Convert.ToString(baska);
+                label1.Text = skor;
+
+  
+            }
+            if (Convert.ToInt32(tiklanan.Tag) != -1 && tiklanan.BackColor == Color.Green)
+            {
+                bitis += 1;
+
+                if (bitis==sonuc)
+                {
+                    MessageBox.Show("Oyun Bitmiştir.\n" +
+                        "Tebrikler");
+                    btnBaslat.Enabled = true;
+                    numericUpDown1.Enabled = true;
+                    baska = 0;
+                    label1.Text = "0";
+                }
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = 63;
+            numericUpDown1.Minimum = 1;
+        }
+    }
+}
